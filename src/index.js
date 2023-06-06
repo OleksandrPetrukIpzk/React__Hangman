a6c4345bcb337e0dec51ab34a641de6f22b59deb
Oleksandr Petruk <ipzk221_poo@student.ztu.ua>
2023-06-06 17:23:27 +0300

    fix ways in index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux";
-import {PersistGate} from "redux-persist/integration/react";
-import {store, persistor} from "./Redux/index";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import {Main} from "Components/Main/Main";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Provider} from "react-redux";
+import {Main} from "Components/Hangman/Main/Main";
 import {Status} from "Components/Statistic/Status/Status";
 import {Shop} from "Components/Shop/Shop";
 import {OwnElements} from "Components/OwnElements/OwnElements";
 import {Sudoku} from "Components/Sudoku/Sudoku";
+import {store, persistor} from "./Redux/index";
+import {PersistGate} from "redux-persist/integration/react";
 
 const router = createBrowserRouter([
     {
@@ -39,7 +39,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-    <RouterProvider router={router}/>
+            <RouterProvider router={router}/>
         </PersistGate>
     </Provider>
 );
